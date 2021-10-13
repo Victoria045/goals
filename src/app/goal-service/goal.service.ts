8c9bd1e09eb13385ecf482f90bba7fefe3797bc1
Victoria045 <victoriaberyl45@gmail.com>
2021-10-13 13:11:11 +0300

    instantiate getGoal

---
+++ src/app/goal-service/goal.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Goals } from '../goal-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoalService {
+
+  getGoals(){
+    return Goals
+  }
+  
+  getGoal(id:any){
+    for (let goal of Goals){
+      if (goal.id == id){
+        return goal;
+      }
+    }
+  }
+
+  constructor() { }
+}
