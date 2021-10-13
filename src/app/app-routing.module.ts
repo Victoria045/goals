f2d5042a4489bc5b68d8b44740b5882cf52935a0
Victoria045 <victoriaberyl45@gmail.com>
2021-10-13 13:06:48 +0300

    configure routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { GoalComponent } from './goal/goal.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:GoalComponent},
+  { path: 'goals', component:GoalComponent},
+  { path: 'about', component:AboutComponent},
+  { path: 'goals/:id', component: GoalDetailComponent },
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
